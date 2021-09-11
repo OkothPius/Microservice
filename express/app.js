a580c250c3fcc2181a781905c3df398a831819d1
Okoth Pius <orukopius8@gmail.com>
2021-09-11 12:04:15 +0300

    feat: update express

---
+++ express/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+//API Endpoints
+//GET    -> api/customers
+//GET    -> api/customers/1
+//PUT    -> api/customers/1
+//DELETE -> api/cutomers/1
+//POST   -> api/customers
+
+app.get('/', (req, res) => {
+	//pass
+});
+
+app.put('/', (req, res) => {
+        //pass
+});
+
+
+app.post('/', (req, res) => {
+        //pass
+});
+
+
+app.delete('/', (req, res) => {
+        //pass
+});
+
