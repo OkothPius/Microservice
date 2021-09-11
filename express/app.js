c8029dfbe492a0965c5a2cd779729b73b48aaf1c
Okoth Pius <orukopius8@gmail.com>
2021-09-11 19:14:05 +0300

    feat: update

--- express/app.js
+++ express/app.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 //API Endpoints
 //GET    -> api/customers
@@ -8,17 +11,47 @@ const app = express();
 //DELETE -> api/cutomers/1
 //POST   -> api/customers
 
+const customers = [
+	{ id: 1, name: 'John' }, 
+	{ id: 2, name: 'Abdalla' }, 
+	{ id: 3, name: 'Naomi' }, 
+	{ id: 4, name: 'Jerry' },
+];
+
 app.get('/', (req, res) => {
-	//pass
+	res.send('Hey There!');
 });
+
+
+app.get('/api/customers', (req, res) => {
+	res.send(customers);
+});
+
+
+app.get('/api/customers/:id', (req, res) => {
+	const customer = customers.find( c => c.id === parseInt(req.params.id));
+	if (!customer) res.status(404).send('The course with the given ID was not found');
+	res.send(customer);
+});
+
 
 app.put('/', (req, res) => {
         //pass
 });
 
 
-app.post('/', (req, res) => {
-        //pass
+app.post('/api/customers', (req, res) => {
+	//Input Validation
+	if (!req.body.name || req.body.name.length < 3){
+		return res.status(400).send("Name is required and should be more than three characters");
+	}
+
+	const customer = {
+		id: customers.length + 1,
+		name: req.body.name,
+	} ;
+	customers.push(customer);
+	res.send(customer);
 });
 
 
@@ -26,3 +59,8 @@ app.delete('/', (req, res) => {
         //pass
 });
 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Listening to port http://localhost:${port}`);
+});
